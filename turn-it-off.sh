20888e6db197ce0e3139eb24462e2487c2779e68
Tran Tam <tranminhtam.10192@gmail.com>
2017-04-23 15:09:53 +0700

    use ssh-config shorthand name for quickly turn off without using password to login

--- turn-it-off.sh
+++ turn-it-off.sh
@@ -4,5 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/user_config.sh
 
-ssh -t $TARGET_USER@$TARGET_IP "sudo shutdown -h now"
-
+if [[ $TARGET_SSH_CONFIG_NAME != "" ]]; then
+	ssh -v -t $TARGET_SSH_CONFIG_NAME "sudo shutdown -h now"
+else
+	ssh -t $TARGET_USER@$TARGET_IP "sudo shutdown -h now"
+fi
